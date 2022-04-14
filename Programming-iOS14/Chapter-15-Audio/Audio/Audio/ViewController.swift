9e9c4acfbe72e2070dc2e47c4f4d921252ffcd35
Mehmet Tarhan <hi@memtarhan.com>
2022-04-14 23:07:24 +0100

    [Audio] - System Sounds

--- Programming-iOS14/Chapter-15-Audio/Audio/Audio/ViewController.swift
+++ Programming-iOS14/Chapter-15-Audio/Audio/Audio/ViewController.swift
@@ -5,15 +5,37 @@
 //  Created by Mehmet Tarhan on 14/04/2022.
 //
 
+import AudioToolbox
 import UIKit
 
 class ViewController: UIViewController {
+    private var playing = false
+
+    private var image: UIImage? {
+        playing ? UIImage(systemName: "stop.fill") : UIImage(systemName: "play.fill")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func didTapPlay(_ sender: UIButton) {
+        playing.toggle()
+        sender.setImage(image, for: .normal)
+        play()
+    }
 
+    private func play() {
+        guard let sndurl = Bundle.main.url(forResource: "500Hz", withExtension: "wav") else { return }
+        var snd: SystemSoundID = 0
+        AudioServicesCreateSystemSoundID(sndurl as CFURL, &snd)
+        AudioServicesPlaySystemSoundWithCompletion(snd) {
+            AudioServicesDisposeSystemSoundID(snd)
+        }
+//        AudioServicesAddSystemSoundCompletion(snd, nil, nil, { snd, _ in
+//            AudioServicesRemoveSystemSoundCompletion(snd)
+//            AudioServicesDisposeSystemSoundID(snd)
+//        }, nil)
+//        AudioServicesPlaySystemSound(snd)
+    }
 }
-
